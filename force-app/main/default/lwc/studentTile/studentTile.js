7867b74e19359664ff8dee5ba6e4f8119b2a2b3f
vanessa <escavanessa@gmail.com>
2024-06-20 20:43:54 -0400

    missing css for the student tile

---
+++ force-app/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from "lwc";
+
+export default class StudentTile extends LightningElement {
+  @api student = {
+    Name: "Vanessa Escamilla",
+    PhotoUrl: "/services/images/photo/003B0FakePictId"
+  };
+
+  @api isSelected = false;
+
+  get tileSelected() {
+    return this.isSelected ? "tile-selected" : "tile";
+  }
+}
