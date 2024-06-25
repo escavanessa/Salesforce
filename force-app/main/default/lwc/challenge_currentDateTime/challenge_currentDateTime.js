acd52d4e6c4883b4f5d902ea208359c7aa687af9
vanessa <escavanessa@gmail.com>
2024-06-24 21:06:05 -0400

    got the date and time to render but the rerender is tough

--- force-app/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
+++ force-app/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
@@ -1,7 +1,31 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class Challenge_currentDateTime extends LightningElement {
-  get currentDateTime() {
-    return new Date().toLocaleString();
+  @track timenow;
+  @track datenow;
+
+  get time() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    let timenow = `${hours}:${minutes}:${seconds}`;
+    this.timenow = timenow;
+    return timenow;
+  }
+
+  get date() {
+    const dateNow = new Date();
+    const month = dateNow.getMonth() + 1;
+    const day = dateNow.getDate();
+    const year = dateNow.getFullYear();
+    let datenow = `${month}/${day}/${year}`;
+    this.datenow = datenow;
+    return datenow;
+  }
+
+  handleClick() {
+    this.timenow = "check1";
+    this.datenow = "check2";
   }
 }
