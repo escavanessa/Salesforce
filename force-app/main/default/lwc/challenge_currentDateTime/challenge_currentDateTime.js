d2385c8e1da220eabd63e5ac468a11202d41af98
vanessa <escavanessa@gmail.com>
2024-07-01 21:00:05 -0400

    tiles wont populate. at a loss

--- force-app/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
+++ force-app/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
@@ -1,18 +1,19 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement } from "lwc";
 
 export default class Challenge_currentDateTime extends LightningElement {
-  @track dateNow
+  //init the date
+  currentDate = new Date();
 
-  connectedCallback(){
-    this.date();
-  }
-
-  get date() {
-    let today = new Date();
-    this.dateNow = today.toISOString();
-  }
-
+  //youre referencing the date created earlier in the handle click - it displays the date (???)
+  //the point of the button is lost on me because the time refreshes every second
   handleClick() {
-    this.date();
+    this.currentDate = new Date();
+  }
+  //the connectedcallback reloads the component everytime the interval happens, showing you the seconds change.
+  connectedCallback() {
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
   }
 }
