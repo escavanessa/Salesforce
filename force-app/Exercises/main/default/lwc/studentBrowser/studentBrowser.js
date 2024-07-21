e648ffac19ae7bcf8b2cb5994f4da220e87c77be
vanessa <escavanessa@gmail.com>
2024-07-21 13:03:06 -0400

    2-7 COMPLETED!!!!!

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,20 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 
 export default class StudentBrowser extends LightningElement {
-    studentList = [];
+  @wire(getStudents, { instructorId: "", courseDeliveryId: ""}) students;
 
-    constructor() {
-        // Call the super constructor here...
-        super()
-        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
-        this.studentList = studentNames.map( (studentName, index) => {
-          return {
-            'sobjectType': 'Contact',
-            'Name': studentName,
-            'PhotoUrl': '/services/images/photo/003B0FakePictId',
-            'Id': index
-          };
-        });
-      }
+  
+    // constructor() {
+    //     // Call the super constructor here...
+    //     super()
+    //     const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+    //     this.studentList = studentNames.map( (studentName, index) => {
+    //       return {
+    //         'sobjectType': 'Contact',
+    //         'Name': studentName,
+    //         'PhotoUrl': '/services/images/photo/003B0FakePictId',
+    //         'Id': index
+    //       };
+    //     });
+    //   }
       
 }
